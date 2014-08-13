bdee52f3c80dd404068a8301b618957027c824cb
Jeremy Kelley <jeremy@33ad.org>
2014-08-13 00:21:34 -0500

    adding some styling to the alerts

--- src/alerts-directive.js
+++ src/alerts-directive.js
@@ -4,6 +4,6 @@ angular.module('ui.bootstrap.alerts')
     restrict: "E",
     replace: true,
     scope: true,
-    template: '<div ng-controller="AlertCtrl" ng-show="anyAlerts"><div class="alert" ng-class="alertClassFor(alert)" ng-repeat="alert in alerts"><button class="close" data-dismiss="alert" ng-click="remove(alert)">×</button><strong>{{alert.title}}</strong> {{alert.message}}</div></div>'
+    template: '<div ng-controller="AlertCtrl" ng-show="anyAlerts"><div class="alert alert-page alert-dark" data-animate="true" ng-class="alertClassFor(alert)" ng-repeat="alert in alerts"><button class="close" data-dismiss="alert" ng-click="remove(alert)">×</button><strong>{{alert.title}}</strong> {{alert.message}}</div></div>'
   };
 });
